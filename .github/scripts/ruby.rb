d27bba89d9855f0e6c554f9218802c2fb07210f9
Itay Brenner <itay@itaysoft.com>
2023-01-30 15:26:55 -0300

    Check string length

--- .github/scripts/ruby.rb
+++ .github/scripts/ruby.rb
@@ -1,4 +1,6 @@
 puts 'COMMIT_SHA is "%s"' % ENV['COMMIT_SHA']
 puts 'GITHUB_SHA is "%s"' % ENV['GITHUB_SHA']
 
-puts 'Ternary is "%s"' % (ENV.has_key?('COMMIT_SHA') ? ENV['GITHUB_SHA'] : ENV['COMMIT_SHA'])
+puts 'Ternary is "%s"' % (ENV.has_key?('COMMIT_SHA') ? ENV['COMMIT_SHA'] : ENV['GITHUB_SHA'])
+
+puts 'Ternary+Check Empty is "%s"' % ((ENV.has_key?('COMMIT_SHA') && ENV['COMMIT_SHA'].length > 0) ? ENV['COMMIT_SHA'] : ENV['GITHUB_SHA'])
