988cbccaeb43f53b837f8260140295259db3261f
noahsmartin <noahm444@gmail.com>
2023-02-01 15:21:57 -0500

    Update AutoCompleteTableViewCell.swift

--- MastodonSDK/Sources/MastodonUI/Scene/ComposeContent/AutoComplete/Cell/AutoCompleteTableViewCell.swift
+++ MastodonSDK/Sources/MastodonUI/Scene/ComposeContent/AutoComplete/Cell/AutoCompleteTableViewCell.swift
@@ -116,6 +116,7 @@ extension AutoCompleteTableViewCell {
         ])
         
         separatorLine.translatesAutoresizingMaskIntoConstraints = false
+        separatorLine.backgroundColor = UIColor.red
         contentView.addSubview(separatorLine)
         NSLayoutConstraint.activate([
             separatorLine.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
